e9825809c237bc7fd9465bcd3020d510683fd638
marciaalegria <ytasegundo@gmail.com>
2019-06-18 12:23:48 -0400

    estructura html cipher inicio dom

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,47 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (mensaje,offset) => {
+   //creando función codificar
+      let nuevaletra="";
+  //creando let para pasar palabras a mayusculas
+      let mensajeEnMayuscula = mensaje.toUpperCase();
+  //crea variable llamada mensaje cifrado
+      let mensajecifrado = "";
+  //creando un for para recorrer mensaje
+      for (let i = 0; i < mensajeEnMayuscula.length; i++){
+        //pidiendo valor ascii
+        let ascii = mensajeEnMayuscula.charCodeAt(i);
+        //crea formula 
+        let formula = ((ascii-65 + offset ) % 26 + 65);
+        nuevaletra = String.fromCharCode(formula);
+  
+        mensajecifrado += nuevaletra;
+  
+      }
+  //nos retorna el mensaje cifrado
+      return mensajecifrado;
+  
+  //console.log(codificar("abcdefghijklmnopqrstuvwxyz",8));
+
     
   },
-  decode: () => {
-    /* Acá va tu código */
+   //creando función decodificar
+  decode: (mensaje,offset) => {
+   //creando el codigo
+      let mensajeEnMayuscula = mensaje.toUpperCase();
+  //crea variable llamada mensaje cifrado
+      let mensajecifrado = "";
+  //creando un for para recorrer mensaje (offset)
+      for (let i = 0; i<mensajeEnMayuscula.length;i++){
+        let ascii = mensajeEnMayuscula.charCodeAt(i);
+        let formula = ((ascii+65 + offset ) % 26 - 65);
+  //
+        let nuevaletra = String.fromCharCode(formula);
+  //
+        mensajecifrado += nuevaletra;
+  
+      }
+  //nos retorna el mensaje cifrado
+      return mensajecifrado;
   }
+  //console.log(codificar("abcdefghijklmnopqrstuvwxyz",8));
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-/*Acá va tu código*/
+
+//guardar el msje que ingrese el usuario
+let message =document.getElementById('espacio1').value;
+console.log (message);
+//guardar boton variable
+const btnSave = document.getElementById('sbtn_1');
+console.log(btnSave);
+//darle un evento al boton 
+btnSave.addEventListener('click' ()=={
+    let message=document.getElementById('espacio1').value
+console.log (message)
+})
