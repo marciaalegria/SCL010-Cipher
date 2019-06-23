63a6ffa6eca79d3115bff0529e65631c14ad6d30
marciaalegria <ytasegundo@gmail.com>
2019-06-22 21:48:14 -0400

    funcion botones

--- src/cipher.js
+++ src/cipher.js
@@ -1,49 +1,60 @@
 
 window.cipher = {
-  encode: (mensaje,offset) => {
-   //creando función codificar
-      let nuevaletra="";
-  //creando let para pasar palabras a mayusculas
-      let mensajeEnMayuscula = mensaje.toUpperCase();
-  //crea variable llamada mensaje cifrado
-  console.log(mensajeEnMayuscula)
-      let mensajecifrado = "";
-  //creando un for para recorrer mensaje
-      for (let i = 0; i < mensajeEnMayuscula.length; i++){
-        //pidiendo valor ascii
-        let ascii = mensajeEnMayuscula.charCodeAt(i);
-        //crea formula 
-        let formula = ((ascii-65 + offset ) % 26 + 65);
-        nuevaletra = String.fromCharCode(formula);
-  
-        mensajecifrado += nuevaletra;
-  
-      }
-  //nos retorna el mensaje cifrado
-      return mensajecifrado;
-  
-  //console.log(codificar("abcdefghijklmnopqrstuvwxyz",8));
-
+    encode: () => {
     
-  },
-   //creando función decodificar
-  decode: (mensaje,offset) => {
-   //creando el codigo
-      let mensajeEnMayuscula = mensaje.toUpperCase();
-  //crea variable llamada mensaje cifrado
-      let mensajecifrado = "";
-  //creando un for para recorrer mensaje (offset)
-      for (let i = 0; i<mensajeEnMayuscula.length;i++){
-        let ascii = mensajeEnMayuscula.charCodeAt(i);
-          let formula = ((ascii-90 - offset ) % 26 + 90);
-  //
-        let nuevaletra = String.fromCharCode(formula);
-  //
-        mensajecifrado += nuevaletra;
-  
-      }
-  //nos retorna el mensaje cifrado
-      return mensajecifrado;
-  }
-  //console.log(codificar("abcdefghijklmnopqrstuvwxyz",8));
-};
+        let offset = document.getElementById("txtCodigoSecreto").value;
+        //console.log(offset);
+    
+        let mensaje = document.getElementById("txtTextoCodificar").value;
+        let nuevaletra="";
+      //creando let para pasar palabras a mayusculas
+          let mensajeEnMayuscula = mensaje.toUpperCase();
+      //crea variable llamada mensaje cifrado
+        //console.log(mensajeEnMayuscula);
+          let mensajecifrado = "";
+      //creando un for para recorrer mensaje
+          for (let i = 0; i <mensajeEnMayuscula.length;i++){
+            //pidiendo valor ascii
+            let ascii = mensajeEnMayuscula.charCodeAt(i);
+            //console.log(ascii);
+            //crea formula 
+            let formula = (ascii-65+offset%26+65);
+            //console.log(formula);
+            nuevaletra = String.fromCharCode(formula);
+      
+            mensajecifrado += nuevaletra;
+      
+          }
+          document.getElementById("espacio4").value=mensajecifrado;
+    
+      //nos retorna el mensaje cifrado
+          //alert(mensajecifrado)
+          //return mensajecifrado;
+          //console.log(mensajecifrado);
+    
+        },
+        decode: () => {
+            let offset = document.getElementById("txtCodigoSecreto").value;
+            let mensaje = document.getElementById("espacio1").value;
+        
+            let mensajeEnMayuscula = mensaje.toUpperCase();
+          //crea variable llamada mensaje cifrado
+              let mensajedescifrado = "";
+          //creando un for para recorrer mensaje (offset)
+              for (let i = 0; i<mensajeEnMayuscula.length;i++){
+                let ascii = mensajeEnMayuscula.charCodeAt(i);
+            
+                let formula = ((ascii-90 - offset ) % 26 + 90);
+          
+                let nuevaletra = String.fromCharCode(formula);
+          
+                mensajedescifrado += nuevaletra;
+          
+              }
+          //nos retorna el mensaje cifrado
+            document.getElementById("espacio2").value=mensajedescifrado;
+        
+             //alert(mensajedescifrado)
+          }
+   
+    };
--- src/index.js
+++ src/index.js
@@ -1,60 +1,24 @@
-function codificar()
-{
-
-    let offset = document.getElementById("txtCodigoSecreto").value;
-    console.log(offset);
-
-    let mensaje = document.getElementById("txtTextoCodificar").value;
-    let nuevaletra="";
-  //creando let para pasar palabras a mayusculas
-      let mensajeEnMayuscula = mensaje.toUpperCase();
-  //crea variable llamada mensaje cifrado
-  console.log(mensajeEnMayuscula);
-      let mensajecifrado = "";
-  //creando un for para recorrer mensaje
-      for (let i = 0; i <mensajeEnMayuscula.length;i++){
-        //pidiendo valor ascii
-        let ascii = mensajeEnMayuscula.charCodeAt(i);
-        console.log(ascii);
-        //crea formula 
-        let formula = (ascii-65+offset%26+65);
-        console.log(formula);
-        nuevaletra = String.fromCharCode(formula);
-  
-        mensajecifrado += nuevaletra;
-  
-      }
-      document.getElementById("espacio4").value=mensajecifrado;
-
-  //nos retorna el mensaje cifrado
-      //alert(mensajecifrado)
-      //return mensajecifrado;
-      //console.log(mensajecifrado);
-
-
+function boton1(){
+    document.getElementById("pantallabienvenida").style.display="none";
+    document.getElementById("pantalla1").style.display="none";
+    document.getElementById("pantalla2").style.display="block";
+}
+function boton5(){
+    document.getElementById("pantallabienvenida").style.display="block";
+    document.getElementById("pantalla1").style.display="block";
+    document.getElementById("pantalla2").style.display="none";
+    document.getElementById("pantalla3").style.display="none"
+}
+function boton2(){
+    document.getElementById("pantallabienvenida").style.display="none";
+    document.getElementById("pantalla1").style.display="none";
+    document.getElementById("pantalla3").style.display="block";
+}
+function boton4(){
+    window.close();
 }
 
-function decodificar()
-{
-    let offset = document.getElementById("txtCodigoSecreto").value;
-    let mensaje = document.getElementById("espacio1").value;
-
-    let mensajeEnMayuscula = mensaje.toUpperCase();
-  //crea variable llamada mensaje cifrado
-      let mensajedescifrado = "";
-  //creando un for para recorrer mensaje (offset)
-      for (let i = 0; i<mensajeEnMayuscula.length;i++){
-        let ascii = mensajeEnMayuscula.charCodeAt(i);
-    
-        let formula = ((ascii-90 - offset ) % 26 + 90);
-  
-        let nuevaletra = String.fromCharCode(formula);
-  
-        mensajedescifrado += nuevaletra;
-  
-      }
-  //nos retorna el mensaje cifrado
-    document.getElementById("espacio2").value=mensajedescifrado;
-
-     //alert(mensajedescifrado)
-}    
+//boton4();
+//boton2();
+//boton5();
+//boton1();
